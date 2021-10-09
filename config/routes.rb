d8f2c796844a0a9cc8e675c409cd2480d5f88553
Dave Burt <dave@burt.id.au>
2021-10-09 13:15:54 +1100

    New Rails 7 app with messages

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :messages, only: [:index, :show, :create, :update, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Almost every application defines a route for the root path ("/") at the top of this file.
+  root "messages#index"
+end
